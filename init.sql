6b3d678d802001685005726b163927959e9c636e
MOLKA296 <98917777+MOLKA296@users.noreply.github.com>
2024-11-15 15:28:10 +0100

    Delete init.sql

--- init.sql
+++
@@ -1,4 +0,0 @@
--- Création de l'utilisateur 'devops_user' et attribution des privilèges
-CREATE USER 'devops_user'@'%' IDENTIFIED BY 'devops_password';
-GRANT ALL PRIVILEGES ON devops.* TO 'devops_user'@'%';
-FLUSH PRIVILEGES;
